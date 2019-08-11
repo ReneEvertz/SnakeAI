66698df62213ffa23fa7bf7278a4406af4f99f65
Jurek Völp <jurvoel@gmx.de>
2019-08-11 21:40:37 +0200

    added drawSnake Method added OnPaint method in the form added gamesettings class

---
+++ SnakeAI/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAI
+{
+	public static class Extension
+	{
+		internal static void DrawSnake(this System.Drawing.Graphics graphics, Snake snake)
+		{
+			graphics.FillRectangle(GameSettings.HeadBrush, snake.Head.X * GameSettings.UnitSize, snake.Head.Y * GameSettings.UnitSize, GameSettings.UnitSize, GameSettings.UnitSize);
+			snake.TailParts.ForEach(x => graphics.FillRectangle(GameSettings.TailBrush, x.X * GameSettings.UnitSize, x.Y * GameSettings.UnitSize, GameSettings.UnitSize, GameSettings.UnitSize));
+		}
+	}
+}
---
+++ SnakeAI/GameSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SnakeAI
+{
+	static class GameSettings
+	{
+		public static readonly int UnitSize;
+		public static readonly System.Drawing.Brush HeadBrush;
+		public static readonly System.Drawing.Brush TailBrush;
+
+		static GameSettings()
+		{
+			if (!File.Exists("config.cfg")) throw new FileNotFoundException();
+			StreamReader strR = new StreamReader("config.cfg", Encoding.UTF8);
+			List<string> configStrings = new List<string>();
+			while (strR.Peek() != -1)
+			{
+				configStrings.Add(strR.ReadLine());
+			}
+			if (configStrings.Count < 2) throw new ArgumentException();
+			UnitSize = Convert.ToInt32(configStrings[0]);
+			HeadBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromName(configStrings[1]));
+			TailBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromName(configStrings[2]));
+		}
+	}
+}
